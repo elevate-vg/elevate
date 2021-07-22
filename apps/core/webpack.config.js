3a0d8c46cc0099bbc30b7d171c8f65d811b3b970
Simon W. Jackson <simon@simonwjackson.com>
2021-07-21 23:13:04 -0500

    chore: set location of prisma binaries

--- apps/core/webpack.config.js
+++ apps/core/webpack.config.js
@@ -2,58 +2,109 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = (env) => {
-   // TODO: Detect env to determine what to pack
-   // const mode = env.mode === 'prod' ? 'prod' : 'dev'
-   // const branch = env.branch ? env.branch : 'develop'
-
-   return {
-      entry: './src/index.ts',
-      target: 'node',
-      externals: ['_http_common'], // in order to ignore all modules in node_modules folder
-      plugins: [
-         new CopyPlugin({
-            patterns: [
-               { from: './prisma/schema.prisma' },
-               { from: '../../libs/electron/', to: './assets/electron/' },
-               { from: '../../node_modules/prisma/query-engine-windows.exe' },
-               { from: '../../node_modules/prisma/query-engine-darwin' },
-            ],
-         }),
-      ],
-      module: {
-         rules: [
-            {
-               test: /\.tsx?$/,
-               use: 'ts-loader',
-               exclude: /node_modules/,
-            },
-            {
-               test: /\.mjs$/,
-               include: /node_modules/,
-               type: 'javascript/auto',
-               // graphql module needs this
-               resolve: {
-                  fullySpecified: false,
-               },
-            },
+module.exports = [
+   // {
+   //    entry: '../../node_modules/prisma/build/index.js',
+   //    target: 'node',
+   //    externals: ['_http_common'], // in order to ignore all modules in node_modules folder
+   //    module: {
+   //       rules: [
+   //          {
+   //             test: /\.tsx?$/,
+   //             use: 'ts-loader',
+   //             exclude: /node_modules/,
+   //          },
+   //          {
+   //             test: /\.mjs$/,
+   //             include: /node_modules/,
+   //             type: 'javascript/auto',
+   //             // graphql module needs this
+   //             resolve: {
+   //                fullySpecified: false,
+   //             },
+   //          },
+   //          {
+   //             test: /\.js$/,
+   //             exclude: /node_modules/,
+   //             use: {
+   //                loader: 'babel-loader',
+   //             },
+   //             // graphql module needs this
+   //             // resolve: {
+   //             //    fullySpecified: false,
+   //             // },
+   //          },
+   //       ],
+   //    },
+   //    mode: 'production',
+   //    optimization: {
+   //       minimize: false,
+   //    },
+   //    output: {
+   //       filename: 'prisma.js',
+   //       path: path.resolve('/Users/simonwjackson/storage/code/elevate/apps/core'),
+   //    },
+   // },
+   (env) => {
+      // TODO: Detect env to determine what to pack
+      // const mode = env.mode === 'prod' ? 'prod' : 'dev'
+      // const branch = env.branch ? env.branch : 'develop'
+      return {
+         // name: 'server',
+         entry: './src/index.ts',
+         target: 'node',
+         externals: ['_http_common'], // in order to ignore all modules in node_modules folder
+         plugins: [
+            new CopyPlugin({
+               patterns: [
+                  { from: './prisma/schema.prisma' },
+                  { from: '../../libs/electron/', to: './assets/electron/' },
+               ],
+            }),
          ],
-      },
-      resolve: {
-         extensions: ['.tsx', '.ts', '.js', '.mjs'],
-         alias: {
-            plugins: path.resolve(__dirname, '../../plugins/'),
-            libs: path.resolve(__dirname, '../../libs/'),
-            themes: path.resolve(__dirname, '../../themes/'),
+         module: {
+            rules: [
+               {
+                  test: /\.tsx?$/,
+                  use: 'ts-loader',
+                  exclude: /node_modules/,
+               },
+               {
+                  test: /\.mjs$/,
+                  include: /node_modules/,
+                  type: 'javascript/auto',
+                  // graphql module needs this
+                  resolve: {
+                     fullySpecified: false,
+                  },
+               },
+               // Prisma: set location of prisma native binaries
+               {
+                  test: /node_modules\/.prisma\/client\/index.js$/,
+                  loader: 'string-replace-loader',
+                  options: {
+                     search: / +?"value": ?".+\/node_modules\/@prisma\/client",?/gm,
+                     replace: '"value":"./bin/prisma",',
+                  },
+               },
+            ],
          },
-      },
-      mode: 'production',
-      optimization: {
-         minimize: false,
-      },
-      output: {
-         filename: 'server.js',
-         path: path.resolve(__dirname, '../../dist'),
-      },
-   }
-}
+         resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.mjs'],
+            alias: {
+               plugins: path.resolve(__dirname, '../../plugins/'),
+               libs: path.resolve(__dirname, '../../libs/'),
+               themes: path.resolve(__dirname, '../../themes/'),
+            },
+         },
+         mode: 'production',
+         optimization: {
+            minimize: false,
+         },
+         output: {
+            filename: 'server.js',
+            path: path.resolve(__dirname, '../../dist'),
+         },
+      }
+   },
+]
