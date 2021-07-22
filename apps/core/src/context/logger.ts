a7da786e2f17ae5ae4005cc4c9e2d801d0f75099
Simon W. Jackson <simon@simonwjackson.com>
2021-07-22 13:30:24 -0500

    chore: add colors to log output

--- apps/core/src/context/logger.ts
+++ apps/core/src/context/logger.ts
@@ -1,5 +1,34 @@
 import { join } from 'path'
-import winston from 'winston'
+import winston, { format } from 'winston'
+import chalk from 'chalk'
+
+const { combine, timestamp, label, printf } = format
+
+const consoleFormat = printf(({ level, message, label }) => {
+   const levelUpper = level.toUpperCase()
+   const levelPadded = ' ' + level + ' '
+
+   switch (levelUpper) {
+      case 'INFO':
+         level = chalk.rgb(0, 0, 0).bgGreenBright.bold(levelPadded)
+         break
+
+      case 'WARN':
+         message = chalk.yellow(message)
+         level = chalk.rgb(0, 0, 0).black.bgYellowBright.bold(levelPadded)
+         break
+
+      case 'ERROR':
+         message = chalk.red(message)
+         level = chalk.rgb(0, 0, 0).black.bgRedBright.bold(levelPadded)
+         break
+
+      default:
+         break
+   }
+
+   return `${chalk.rgb(0, 0, 0).bgBlue.bold(' ' + label + ' ')}${level}  ${message}`
+})
 
 export default (dir: string) => {
    const logger = winston.createLogger({
@@ -20,7 +49,12 @@ export default (dir: string) => {
    if (process.env.NODE_ENV !== 'production') {
       logger.add(
          new winston.transports.Console({
-            format: winston.format.simple(),
+            format: combine(
+               label({ label: 'elevate' }),
+               timestamp(),
+               format.splat(),
+               consoleFormat,
+            ),
          }),
       )
    }
