35734decfd43cb1861f8b7d57fa6dc052f8f70e3
Simon W. Jackson <github@simonwjackson.io>
2025-06-09 19:50:02 -0500

    refactor: rename webview-src to ui

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteSingleFile } from 'vite-plugin-singlefile';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import path from "path";
 
 export default defineConfig({
-  root: 'webview-src',
-  build: {
-    outDir: '../assets/web',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'webview-src/index.html')
-    }
-  },
-  plugins: [react(), viteSingleFile()],
-  esbuild: {
-    jsx: 'automatic'
-  }
+	root: "ui",
+	build: {
+		outDir: "../assets/web",
+		emptyOutDir: true,
+		rollupOptions: {
+			input: path.resolve(__dirname, "ui/index.html"),
+		},
+	},
+	plugins: [react(), viteSingleFile()],
+	esbuild: {
+		jsx: "automatic",
+	},
 });
